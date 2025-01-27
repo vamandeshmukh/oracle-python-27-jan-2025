e4f961fbfb3ed3b085c4d716ad7ca084afbbf8c8
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-01-27 10:04:34 +0530

    first commit

--- day1/script.py
+++ day1/script.py
@@ -1,1 +1,34 @@
-print('Hello')
+
+# day 1
+
+# java c c++
+# int num = 10;
+
+# python 
+# num = 10 
+
+# eid = 101
+# first_name = 'Sonu'
+# salary = 10.25
+
+# print(eid, first_name, salary)
+
+# camelCase 
+# PascleCase 
+# snake_case 
+
+# identifier -  C++           python 
+# class name -  ClassName     ClassName  
+# method name - methodName    method_name function_name 
+# field name - fildName       field_name 
+# file name - SameAsClassName.java file-name.py 
+# package name  - package.name packagename 
+
+# program to add two integers and print the sum , three vriables 
+
+
+
+
+
+
+
