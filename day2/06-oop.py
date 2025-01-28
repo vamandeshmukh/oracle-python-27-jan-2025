54f66ddda85f6c432a27dd93f118867ef0237201
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-01-28 15:35:51 +0530

    day2

--- day2/06-oop.py
+++ day2/06-oop.py
@@ -72,58 +72,103 @@
 # emp2 = Employee(102, 'Monu')
 # print(emp2.id, emp2.name)
 
-# Encapsulation 
+# # Encapsulation 
 
-class Employee :
+# class Employee :
     
-    def __init__(self, id, name, salary):
-        self.id = id 
-        self.name = name 
-        self.__salary = salary # __private field 
+#     def __init__(self, id, name, salary):
+#         self.id = id 
+#         self.name = name 
+#         self.__salary = salary # __private field 
         
-    def set_salary(self, salary): # setter 
-        self.__salary = salary
+#     def set_salary(self, salary): # setter 
+#         self.__salary = salary
     
-    def get_salary(self): # getter 
-        return self.__salary 
+#     def get_salary(self): # getter 
+#         return self.__salary 
     
-    def emp_data(self):
-        print(self.id, self.name)
+#     def emp_data(self):
+#         print(self.id, self.name)
 
-emp2 = Employee(102, 'Monu', 10.25)
-print(emp2.id, emp2.name, emp2.get_salary())
-# emp2.__salary = 12.50 # error 
+# emp2 = Employee(102, 'Monu', 10.25)
+# print(emp2.id, emp2.name, emp2.get_salary())
+# # emp2.__salary = 12.50 # error 
 
-print(emp2._Employee__salary) # possible but not recommenred 
-emp2.set_salary(12.50)
-print(emp2.id, emp2.name, emp2.get_salary())
+# print(emp2._Employee__salary) # possible but not recommenred 
+# emp2.set_salary(12.50)
+# print(emp2.id, emp2.name, emp2.get_salary())
 
-# Inheritance  
+# # Inheritance  
 
-# sub class extends super class 
-class ContractualEmployee (Employee):
+# # sub class extends super class 
+# class ContractualEmployee (Employee):
     
-    # subclass has access to properties of super class 
-    # subclass can create its own functions as well 
-    def show_bonus(self):
-        print(self.get_salary() + 10)
+#     # subclass has access to properties of super class 
+#     # subclass can create its own functions as well 
+#     def show_bonus(self):
+#         print(self.get_salary() + 10)
         
-# polymorphism - method overriding 
-    def emp_data(self):
-        print(self.id, self.name, self.get_salary())
+# # polymorphism - method overriding 
+#     def emp_data(self):
+#         print(self.id, self.name, self.get_salary())
 
 
-emp3 = ContractualEmployee(103, 'Ponu', 15.25)
-emp3.emp_data()
-emp3.show_bonus()
+# emp3 = ContractualEmployee(103, 'Ponu', 15.25)
+# emp3.emp_data()
+# emp3.show_bonus()
 
 # Polymorphism 
 
 
+# class Class:
+#     def __init__(self, id, salary, name):
+#         self.id = id # public field 
+#         self._salary = salary # protected field - accessible in the child class 
+#         self.__name = name # private field 
+
+# class ChildClass (Class) :
+#     pass 
+
+# cls = Class(1, 10, 'a')
+
+# # print(cls.__name) # not possible 
+# # print(cls._Class__name) # name mangling, pollisble but mot recommended  
+# # print(cls._salary) # pollisble but mot recommended  
+
+# cls2 = ChildClass(2, 20, 'b')
+# # print(cls2._salary) # pollisble but mot recommended  
 
 
+# # __init__ (constructor)  __str__ (toString())  __gt__ __lt__ __eq__ 
+# # double underscore methods == dunder methods == magic methods 
+
+# class Employee:
+    
+#     def __init__(self, id, name, salary):
+#         self.id = id 
+#         self.name = name 
+#         self.__salary = salary # __private field 
+        
+#     def set_salary(self, salary): # setter 
+#         self.__salary = salary
+    
+#     def get_salary(self): # getter 
+#         return self.__salary 
+    
+#     def __str__(self):
+#         return f'The employee id is {self.id} name is {self.name} and salary is {self.__salary}.'
+    
+#     def emp_data(self):
+#         print(self.id, self.name)
+
+# emp2 = Employee(102, 'Monu', 10.25)
+# # print(emp2)
+# print(str(emp2)) # method call - str(object_name)
 
 
+salary = 10.25
+
+FINAL_FIELD = 10
 
 
 
