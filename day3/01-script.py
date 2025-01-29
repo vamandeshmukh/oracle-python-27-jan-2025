fe4592a5e73288135e15efa4dd5f878d3fcfcbbc
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-01-29 10:08:47 +0530

    day3

--- day3/01-script.py
+++ day3/01-script.py
@@ -1,3 +1,35 @@
 # python 01-script.py 
 
-print('')
+# double underscore == dunder 
+
+# operator overloading (using magic methods)
+
+class Employee :
+    
+    def __init__(self, id, name):
+        self.id = id 
+        self.name = name 
+
+    def __eq__(self, other):
+        # return self.id == other.id     
+        return self.id == other.id and self.name == other.name
+    
+    def emp_data(self):
+        print(self.id, self.name)
+    
+emp1 = Employee(101, 'Sonu')
+emp2 = Employee(101, 'Sonu')
+
+print(emp1.id, emp1.name)
+print(emp2.id, emp2.name)
+print(emp1 == emp2)
+# print(emp1.__eq__(emp2))
+# print(emp1 > emp2) 
+# print(emp1 + emp2)
+
+
+
+
+
+
+
