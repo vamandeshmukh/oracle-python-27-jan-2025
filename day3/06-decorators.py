c6d9d956cb338d4280a0e586f03d471049efede9
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-01-29 16:22:15 +0530

    message

--- day3/06-decorators.py
+++ day3/06-decorators.py
@@ -1,28 +1,51 @@
+# # Decorators in Python 
+
+# # custom decorator 
+# def data_accessed(func):
+#     def wrapper(*args):
+#             print('emp data accessed.')
+#             return func(*args)
+#     return wrapper
+
+# # decorator used in this class 
+# class Employee :
+    
+#     def __init__(self, id, name):
+#         self.id = id 
+#         self.name = name 
+
+#     # @property #built-in decorator in Python 
+#     # def get_name(self):
+#     #     return self.name
+    
+#     @data_accessed
+#     def emp_data(self):
+#         print(self.id, self.name)
+    
+# emp1 = Employee(101, 'Sonu')
+# emp1.emp_data()
+# # print(emp1.get_name)
+
+
 # Decorators in Python 
 
-# custom decorator 
 def data_accessed(func):
     def wrapper(*args):
             print('emp data accessed.')
+            print(args)
             return func(*args)
     return wrapper
 
-# decorator used in this class 
 class Employee :
     
     def __init__(self, id, name):
         self.id = id 
         self.name = name 
 
-    # @property #built-in decorator in Python 
-    # def get_name(self):
-    #     return self.name
-    
     @data_accessed
     def emp_data(self):
         print(self.id, self.name)
     
 emp1 = Employee(101, 'Sonu')
 emp1.emp_data()
-# print(emp1.get_name)
 
