f8538989bd877a285b3587be4d388bcbe06991ea
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-01-29 11:20:52 +0530

    day3

--- day3/02-read-file.py
+++ day3/02-read-file.py
@@ -24,3 +24,20 @@ with open(file2, 'r') as f2:
 #     t2 = f2.read()
 #     print((t2))
 
+
+
+
+# file_name = 'sample.txt'
+
+# with open(file_name, 'r') as file:
+#     # text = file.read()
+    
+#     for line in file:
+#         if line.strip() == 'line 2':
+#             continue
+#         print(line.strip())
+    
+    
+    
+
+
