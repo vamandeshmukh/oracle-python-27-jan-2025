64b71805038a794dcd16d8bd1763e7396a5c09c1
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-01-30 10:06:30 +0530

    day4

--- day4/01-numpy-intro.py
+++ day4/01-numpy-intro.py
@@ -1,16 +1,46 @@
 import numpy as np 
 
-# create array using np
+# # create array using np
+# arr = np.array([25, 31, 22, 9, 17])
+# print(arr)
+# print(arr[0]) # access elements using index 
+
+# # arrays with 0s and 1s
+# zer_arr =np.zeros(5)
+# print(zer_arr)
+# one_arr =np.ones(5)
+# print(one_arr)
+
+# # array with a range 
+# range_arr = np.arange(10)
+# print(range_arr)
+
+# slicing 
+# arr = np.array([25, 31, 22, 9, 17])
+# print(arr)
+# sliced_arr = arr[1:4]
+# print(sliced_arr)
+
+# boradcasting 
+# brd_arr = arr + 10 
+# print(brd_arr)
+
+# statistical operations 
 arr = np.array([25, 31, 22, 9, 17])
 print(arr)
-print(arr[0])
 
-# arrays with 0s and 1s
-zer_arr =np.zeros(5)
-print(zer_arr)
-one_arr =np.ones(5)
-print(one_arr)
+# mean 
+mean_arr = np.mean(arr)
+print(mean_arr)
+# median 
+med_arr = np.median(arr)
+print(med_arr)
+# standard deviation 
+st_dev = np.std(arr)
+print(st_dev)
 
-# array with a range 
-range_arr = np.arange(10)
-print(range_arr)
+
+
+
+
+
