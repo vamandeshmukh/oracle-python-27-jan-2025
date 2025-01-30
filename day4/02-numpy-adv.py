1f46fa56e266f86075048b121d31f335544483dd
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-01-30 11:27:49 +0530

    day4

--- day4/02-numpy-adv.py
+++ day4/02-numpy-adv.py
@@ -5,5 +5,15 @@ import numpy as np
 
 api_url = 'https://jsonplaceholder.typicode.com/posts'
 response = rq.get(api_url)
+
 arr = np.array([d['title'] for d in response.json()])
-print(arr)
+# print(arr)
+# print(arr.size)
+# print(arr.shape)
+# print(arr.dtype)
+# print(np.char.upper(arr))
+# print(np.char.find(arr, 'e'))
+# print(np.char.split(arr, ' '))
+#  "title": "at nam consequatur ea labore ea harum",
+# list(['at', 'nam', 'consequatur', 'ea', 'labore', 'ea', 'harum'])]
+print(np.sort(arr))
