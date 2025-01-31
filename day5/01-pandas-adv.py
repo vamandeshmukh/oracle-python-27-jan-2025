a113114cd4012497ec300e383efe00eabce72802
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-01-31 10:12:55 +0530

    day 5

--- day5/01-pandas-adv.py
+++ day5/01-pandas-adv.py
@@ -1,3 +1,33 @@
+
+import requests as rq 
 import pandas as pd 
 
-print(pd)
+key_path = r'D:\VamanPro\Codes\oracle-jan-2025\keys\alpha-vantage-api-key.txt'
+
+with open(key_path, 'r') as file:
+    api_key = file.read()
+
+symbol = 'SBIN'
+market = 'BSE'
+api_url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}.{market}&outputsize=full&apikey={api_key}'
+
+response = rq.get(api_url)
+data = response.json()
+# print(data)
+
+
+# show last ten records 
+
+time_series = data.get("Time Series (Daily)", {}) 
+
+df = pd.DataFrame.from_dict(time_series, orient="index")
+
+df.columns = ["Open", "High", "Low", "Close", "Volume"]
+
+df.index = pd.to_datetime(df.index)
+df.index.name = "Date"
+
+last_ten_records = df.sort_index().tail(10)
+first_ten_records = df.sort_index().head(10)
+
+print(last_ten_records)
